f6627b3db0e4b84e8131a1121c3757aeade2b7f9
MaxBardyn <maxbardyn@gmail.com>
2020-10-31 20:19:10 +0200

    add csv data reader

---
+++ src/main/java/lab1/Main.java
@@ -0,0 +1,34 @@
+package lab1;
+
+import java.util.List;
+
+import static lab1.SortMethods.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Insect> dataFromFile = WriteAndReadCsvFile.getDataFromFile("insect.csv");
+
+        selectionSortInsectsBySpeedDesc(dataFromFile);
+
+        System.out.println("Selection Algorithm");
+        System.out.println("Work time: " + selectionTimeInMillis);
+        System.out.println("Swaps: " + selectionSwapCounter);
+        System.out.println("Comparisons: " + selectionCompareCounter);
+        for (Insect insect : dataFromFile) {
+            System.out.println(insect);
+        }
+
+        System.out.println("========================================================================");
+
+        quickSortInsectByWeightDesc(dataFromFile);
+
+        System.out.println("Quick Algorithm");
+        System.out.println("Work time: " + quickTimeInMillis);
+        System.out.println("Swaps: " + quickSwapCounter);
+        System.out.println("Comparisons: " + quickCompareCounter);
+        for (Insect insect : dataFromFile) {
+            System.out.println(insect);
+        }
+    }
+}
---
+++ src/main/java/lab1/WriteAndReadCsvFile.java
@@ -0,0 +1,66 @@
+
+package lab1;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteAndReadCsvFile {
+
+
+    protected static List<Insect> getDataFromFile(String fileName) {
+
+        var file = new File(fileName);
+        List<Insect> insectList = new ArrayList<>();
+
+        if (!file.exists()) {
+            createFileWithInsectData(fileName);
+        }
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(file))) {
+
+            String line;
+            fileReader.readLine();
+
+            while ((line = fileReader.readLine()) != null) {
+                //Get all tokens available in line
+                String[] tokens = line.split(",");
+                insectList.add(new Insect(tokens[0], Float.parseFloat(tokens[1]), Float.parseFloat(tokens[2])));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return insectList;
+    }
+
+    private static void createFileWithInsectData(String fileName) {
+
+        var insectList = createInsectList();
+
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
+
+            fileWriter.write(Insect.getHeaders() + "\n");
+
+            for (Insect insect : insectList) {
+                fileWriter.write(insect.toCSV());
+                fileWriter.write("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<Insect> createInsectList() {
+        List<Insect> insectList = new ArrayList<>();
+        insectList.add(new Insect("Insect1", 1F, 3F));
+        insectList.add(new Insect("Insect2", 2F, 25F));
+        insectList.add(new Insect("Insect3", 0.5F, 31F));
+        insectList.add(new Insect("Insect4", 0.7F, 3F));
+        insectList.add(new Insect("Insect5", 0.1F, 30F));
+        insectList.add(new Insect("Insect6", 5F, 23F));
+        insectList.add(new Insect("Insect7", 0.3F, 30F));
+        insectList.add(new Insect("Insect8", 0.7F, 60F));
+        insectList.add(new Insect("Insect9", 0.4F, 12F));
+        insectList.add(new Insect("Insect10", 1F, 18F));
+        return insectList;
+    }
+}
